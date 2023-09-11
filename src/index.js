dd14f39dfeb3ce2a204777073939f02762f18c97
Hiuzt <48331282+Hiuzt@users.noreply.github.com>
2023-09-11 16:01:33 +0200

    asd

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter, Switch, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// import $ from "jquery"
 import store from "./redux/store"
 import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
+    <>
+        asd
+   
         <BrowserRouter>
             <Routes>
                 <Route path="/*" element={            
@@ -19,7 +20,7 @@ root.render(
                 } />
             </Routes>
         </BrowserRouter>
-        // <App />
+        </>
    
         
 );
