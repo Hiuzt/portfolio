3482a59355905412b96fecd49d164ad7d9e5b93d
Hiuzt <48331282+Hiuzt@users.noreply.github.com>
2023-09-11 17:30:30 +0200

    asd

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
-        <App />   
+    <BrowserRouter>  
+        <Provider store={store}>
+            <App />                
+        </Provider>
     </BrowserRouter>
     //          
-    //     {/* <Provider store={store}> */}
-    //                      
-    //     {/* </Provider> */}
+
     // 
    
         
